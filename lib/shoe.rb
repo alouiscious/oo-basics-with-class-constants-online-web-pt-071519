ebca9037f52c61fc5e1428ac481af1f53cf461f6
Renee Sims <info@reneesoul.com>
2019-08-09 19:49:23 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -8,13 +8,14 @@ class Shoe
   
   def initialize(brand)
     @brand = brand
+    BRANDS << brand
     
   end
   
   def brand=(brand)
       @brand = brand
-      BRANDS << brand
-      BRANDS.uniq!
+binding.pry      
+      BRANDS = BRANDS.uniq!
   end
   
 
